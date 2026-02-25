391a97e920bf7c5c8c9d05325a654237584b2910
Hasere Mucadele <haseremucadele@example.com>
2026-02-25 09:47:24 +0300

    Updates

--- _next/static/chunks/d75944ce0c371019.js
+++
@@ -1,1 +0,0 @@
-(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,99348,e=>{"use strict";let t=(0,e.i(44641).default)("loader-circle",[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]]);e.s(["Loader2",()=>t],99348)},75880,(e,t,o)=>{t.exports=e.r(14460)}]);
